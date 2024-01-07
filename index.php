ee46573f96a144fab12fcb781705292c10968602
alphaonix <alphaonix@discente.ufg.br>
2024-01-07 01:40:43 -0300

    Add Styling the main container

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
 <head>
     <title>CloneTube</title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="assets/js/commonActions.js"></script>
 
 </head>
 <body>
@@ -11,7 +17,7 @@
     <div id="pageContainer">
 
         <div id="mastHeadContainer">
-            Hello
+            <button class="navShowHide">Click me!</button>
         </div>
 
         <div id="sideNavContainer">
@@ -20,6 +26,10 @@
 
         <div id="mainSectionContainer">
             
+            <div id="mainContentContainer">
+                hello
+            </div>
+
         </div>
 
     </div>
