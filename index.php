9354643f66838aff67d1573fc2c644fe9f1de7a7
alphaonix <alphaonix@discente.ufg.br>
2024-01-07 03:00:16 -0300

    Creating the search bar

--- index.php
+++ index.php
@@ -25,6 +25,15 @@
                 <img src="assets/images/icons/clonetube.png">
             </a>
                 
+            <div class="searchBarContainer">
+                <form action="search.php" method="GET">
+                    <input type="text" class="searchBar" name="term" placeholder="Search...">
+                    <button class="searchButton">
+                        <img src="assets/images/icons/search.png">
+                    </button>
+                </form>
+            </div>
+
         </div>
 
         <div id="sideNavContainer" style="display:none;">
